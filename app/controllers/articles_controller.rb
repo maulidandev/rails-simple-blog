dd761bf0550f08f44dd2f474990d66307d86b33d
Maulidan Nashuha <maulidanms@gmail.com>
2020-09-21 12:15:10 +0700

    edit and update article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,9 +16,23 @@ class ArticlesController < ApplicationController
             render 'new'
         end
     end
-       
+
     def show
         @article = Article.find(params[:id])
+    end
+       
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+       
+        if @article.update(article_params)
+          redirect_to @article
+        else
+          render 'edit'
+        end
     end
 
     private
