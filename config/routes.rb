7031cf42e33b428de383f5e206e0aa851c01dd8c
Maulidan Nashuha <maulidanms@gmail.com>
2020-09-22 12:39:16 +0700

    bootstrap style

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  root "welcome#index"
+  root "welcome#index", as: "home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
